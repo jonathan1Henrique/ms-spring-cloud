cdda4277b2869226c914b6e60ac119f8894fa92d
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-08-19 20:42:32 -0300

    Projeto api gateway zuul criado

---
+++ gateway-zuul/src/test/java/com/blackti/gateway/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackti.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
