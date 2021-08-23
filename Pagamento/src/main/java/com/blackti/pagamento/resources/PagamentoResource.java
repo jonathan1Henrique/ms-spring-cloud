aa5736499b78dc44dc9dd29e7f02d94234dd04ed
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-08-22 21:11:43 -0300

    configurações de refresh atualizadas

--- Pagamento/src/main/java/com/blackti/pagamento/resources/PagamentoResource.java
+++ Pagamento/src/main/java/com/blackti/pagamento/resources/PagamentoResource.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/pagamento")
 public class PagamentoResource {
