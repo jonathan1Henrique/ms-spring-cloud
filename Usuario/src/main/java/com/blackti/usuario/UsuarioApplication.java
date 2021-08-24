6aa89827ef611b9b04aee394d7b723c02c9e0572
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-08-24 20:49:19 -0300

    Configuração do projeto usuario

--- Usuario/src/main/java/com/blackti/usuario/UsuarioApplication.java
+++ Usuario/src/main/java/com/blackti/usuario/UsuarioApplication.java
@@ -2,7 +2,9 @@ package com.blackti.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UsuarioApplication {
 
